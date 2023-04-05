3efee6b8048c29d0b2da5940081f01518a16968a
jjcmerin <jjc.merin@mail.ru>
2023-04-05 12:46:19 +0700

    add reading from .dat file for tetmesh

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from utils.mesh_writer import write_mesh
 
 
 def main():
-    tetmesh = TetMesh.from_file('kube.vol')
-    facemesh = FaceMesh.from_file('kube.vol')
-    write_mesh("out.vol", tetmesh, facemesh)
+    tetmesh = TetMesh.read_from_file('cube.dat')
+    facemesh = FaceMesh.read_from_file('cube.dat')
+    write_mesh("out.vol", facemesh, tetmesh)
 
 
 if __name__ == '__main__':
