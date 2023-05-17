41421c1382f948bf60406da212444f6fe7d67b9c
jjcmerin <jjc.merin@mail.ru>
2023-05-17 18:36:15 +0700

    update(exceptions/surface_errors): add a new exc

--- exceptions/surface_errors.py
+++ exceptions/surface_errors.py
@@ -18,3 +18,14 @@ class NonequivalentPointsCountError(Exception):
 
     def __str__(self):
         return f'Количество точек на плоскостях {self.srf1} и {self.srf2} не совпадает. Укажите корректные плоскости'
+
+
+class SurfacesNotFoundError(Exception):
+    def __init__(self, srf1, srf2):
+        self.srf1 = srf1
+        self.srf2 = srf2
+        self.message = 'Указанные поверхности не обнаружены'
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'Одна или обе из указанных поверхностей ({self.srf1} и {self.srf2}) не обнаружены. Укажите существующие'
